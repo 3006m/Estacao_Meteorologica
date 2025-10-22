6daa71fcb722cc89e8ff1406c67b856bd4d84df6
Murilo Correia <mucorreia008@gmail.com>
2025-10-22 15:32:23 -0300

    Versão 2

--- Manhattan/src/pages/equipe.jsx
+++ Manhattan/src/pages/equipe.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import "../styles/css.css";
 import FelipeImg from "../assets/felipe.jpg";
 import CorreiaImg from "../assets/correia.jpg";
@@ -66,6 +67,7 @@ export default function Equipe() {
           <img src={Equipa} alt="Equipe." className="equipeTotalI" />
         </div>
       </div>
+      <Footer/>
     </main>
   );
 }
